3402f9c71c51941256d4cbfbba06f170d9ec6e0b
DangouHaim <gholub1996@mail.ru>
2017-03-12 14:11:14 +0300

    commit1

---
+++ NN/Neuron.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace NN
+{
+    public class Neuron
+    {
+        public double[] _in;
+        public double[] _out;
+        public double[,] _wio;
+        double[] _lim;
+        double _speed = 0.1;
+
+
+
+        public Neuron(int inputs, int outputs, double[] limits, double speed)
+        {
+            _in = new double[inputs];
+            _out = new double[outputs];
+            _wio = new double[inputs, outputs];
+            SetWeights();
+            _lim = limits;
+            Array.Sort(_lim);
+            _speed = speed;
+        }
+
+        void SetWeights()
+        {
+            Random r = new Random();
+
+            for(int i = 0; i < _in.Length; i++)
+            {
+                for(int j = 0; j < _out.Length; j++)
+                {
+                    _wio[i, j] = r.NextDouble() * 0.3 + 0.1;
+                }
+            }
+        }
+
+        public void SetIn(double[] IN)
+        {
+            for(int i = 0; i < _in.Length; i++)
+            {
+                _in[i] = IN[i];
+            }
+        }
+
+        public void Out()
+        {
+            for(int i = 0; i < _out.Length; i++)
+            {
+                _out[i] = 0;
+                for(int j = 0; j < _in.Length; j++)
+                {
+                    _out[i] += _in[j] * _wio[j, i];
+                }
+
+                double o = 0;
+                for(int j = 1; j < _lim.Length; j++)
+                {
+                    if(_out[i] > _lim[j] / 2)
+                    {
+                        o = _lim[j];
+                    }
+                }
+                _out[i] = o;
+            }
+        }
+
+        public void Study(double[] error)
+        {
+            for(int i = 0; i < _in.Length; i++)
+            {
+                for(int j = 0; j < _out.Length; j++)
+                {
+                    _wio[i, j] += _speed * _in[i] * error[j];
+                }
+            }
+        }
+    }
+}
---
+++ NN/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[][] IN = new double[][]
+            {
+                new double[] { 0, 1 },
+                new double[] { 1, 0 },
+                new double[] { 1, 1 },
+                new double[] { 0, 0 }
+            };
+            double[] OUT = new double[]
+            {
+                1,
+                1,
+                1,
+                0
+            };
+
+            Neuron n = new Neuron(2, 1, new double[] { 0, 1 }, 0.1);
+            double ge = 0;
+            do
+            {
+                ge = 0;
+                for(int i = 0; i < IN.Length; i++)
+                {
+                    n.SetIn(IN[i]);
+                    n.Out();
+                    List<double> le = new List<double>();
+                    for(int j = 0; j < n._out.Length; j++)
+                    {
+                        le.Add(OUT[i] - n._out[j]);
+                        ge += Math.Abs(le[j]);
+                    }
+                    n.Study(le.ToArray());
+                }
+            }
+            while (ge > 0);
+            Console.WriteLine("complate");
+            foreach(var v in IN)
+            {
+                n.SetIn(v);
+                n.Out();
+                foreach(var vv in n._out)
+                {
+                    Console.WriteLine(vv);
+                }
+            }
+            Console.ReadLine();
+
+            IN = new double[][]
+            {
+                new double[] { 0, 1 },
+                new double[] { 1, 0 },
+                new double[] { 1, 1 },
+                new double[] { 0, 0 }
+            };
+            OUT = new double[]
+            {
+                1,
+                0.5,
+                1.255,
+                0
+            };
+
+            n = new Neuron(2, 8, new double[] { 0, 1.255, 0.5, 1 }, 0.1);
+            Neuron nn = new Neuron(8, 1, new double[] { 0, 1.255, 0.5, 1 }, 0.1);
+
+            ge = 0;
+            do
+            {
+                Console.WriteLine(ge);
+                ge = 0;
+                for(int i = 0; i < IN.Length; i++)
+                {
+                    n.SetIn(IN[i]);
+                    n.Out();
+                    nn.SetIn(n._out);
+                    nn.Out();
+
+                    List<double> le = new List<double>();
+                    foreach(double d in nn._out)
+                    {
+                        le.Add(OUT[i] - d);
+                        ge += Math.Abs(OUT[i] - d);
+                    }
+                    List<double> he = new List<double>();
+
+                    for(int j = 0; j < le.Count; j++)
+                    {
+                        for(int k = 0; k < nn._in.Length; k++)
+                        {
+                            he.Add(le[j] * nn._wio[k, j]);
+                        }
+                    }
+
+                    nn.Study(le.ToArray());
+                    n.Study(he.ToArray());
+
+                }
+            }
+            while (ge > 0);
+
+            Console.WriteLine("OK");
+            foreach(var v in IN)
+            {
+                n.SetIn(v);
+                n.Out();
+                nn.SetIn(n._out);
+                nn.Out();
+                foreach(var vv in nn._out)
+                {
+                    Console.WriteLine(vv);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
